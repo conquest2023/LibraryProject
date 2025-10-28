517d7c89668484d075fd3593936ebed4ee631f10
conquest2023 <dlqns928@naver.com>
2025-10-28 20:31:28 +0900

    test

--- src/main/java/project/library/service/CalculateServiceImpl.java
+++ src/main/java/project/library/service/CalculateServiceImpl.java
@@ -45,7 +45,6 @@ public class CalculateServiceImpl implements CalculateService{
 
     private static final String API_BASE_URL = "http://data4library.kr/api";
 
-
     private static final String AUTH_KEY = "1df2f040d9555558e014f541e2908356008ca9e3aa7a1d9c43ec2c15e54f5f4b";
     @Override
     public List<NearestLibraryDetail> calculateDistance(LocationDto locationDto) {
