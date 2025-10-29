18a70a9132d42b3374c7076003593b26439030d7
conquest2023 <dlqns928@naver.com>
2025-10-29 20:15:02 +0900

    FEAT-크롤링 목차 가져오기 성공

--- src/test/java/project/library/CrawlingTest.java
+++ src/test/java/project/library/CrawlingTest.java
@@ -1,28 +1,28 @@
-package project.library;
-
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import project.library.service.CalculateService;
-import project.library.service.CrawlingService;
-import project.library.service.CrawlingServiceImpl;
-
-import java.io.IOException;
-
-@SpringBootTest
-public class CrawlingTest {
-
-
-    private final static String URL ="https://search.kyobobook.co.kr/search?keyword=";
-
-    @Test
-    void 크롤링테스트() throws IOException {
-
-        CrawlingService crawlingService=new CrawlingServiceImpl();
-
-        String newWord = URL.concat("컴퓨터밑바닥비밀");
-        crawlingService.crawlData(newWord);
-    }
-
-
-}
+//package project.library;
+//
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import project.library.service.CalculateService;
+//import project.library.service.CrawlingService;
+//import project.library.service.CrawlingServiceImpl;
+//
+//import java.io.IOException;
+//
+//@SpringBootTest
+//public class CrawlingTest {
+//
+//
+//    private final static String URL ="https://search.kyobobook.co.kr/search?keyword=";
+//
+//    @Test
+//    void 크롤링테스트() throws IOException {
+//
+//        CrawlingService crawlingService=new CrawlingServiceImpl();
+//
+//        String newWord = URL.concat("컴퓨터밑바닥비밀");
+//        crawlingService.crawlData(newWord);
+//    }
+//
+//
+//}
