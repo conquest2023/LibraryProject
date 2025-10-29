847d1eea4463aae80e2eb74f80d876eaccaf4880
conquest2023 <dlqns928@naver.com>
2025-10-29 20:20:12 +0900

    BUGFIX-crawling Controller 경로명 추가

--- src/main/java/project/library/controller/CrawlingController.java
+++ src/main/java/project/library/controller/CrawlingController.java
@@ -4,6 +4,7 @@ package project.library.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import project.library.service.CrawlingService;
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.Set;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class CrawlingController {
 
